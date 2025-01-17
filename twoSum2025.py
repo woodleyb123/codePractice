6fb815d0f54eb48281f9055fa41c7868ee1da570
woodleyb123 <woodley.burrow1@gmail.com>
2025-01-17 16:51:41 -0500

    New solution

--- twoSum2025.py
+++ twoSum2025.py
@@ -1,6 +1,7 @@
 from typing import List
 
 class Solution:
+    # Two Pass
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         newDict = {}
         for i, n in enumerate(nums):
@@ -10,5 +11,17 @@ class Solution:
             diff = target - n
             if diff in newDict and newDict[diff] != i:
                 return [i, newDict[diff]]
+            
+    # One Pass
+
+    def twoSumOnePass(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in map and map[diff] != i:
+                return [map[diff], i]
+            else:
+                map[n] = i
+
     
-print(Solution.twoSum(Solution, [3,4,5,6], 7))
+print(Solution.twoSumOnePass(Solution, [3,4,5,6], 7))
