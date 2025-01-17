a4842569980b2909e4ac0a576e292832dc444fde
woodleyb123 <woodley.burrow1@gmail.com>
2025-01-17 18:14:32 -0500

    Cleaned up solution

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -44,5 +44,21 @@ class Solution:
                 suffixVal *= n
 
         return output
+    
+    def productExceptSelfThree(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        prefix = 1
 
-print(Solution.productExceptSelfTwo(Solution, [-1,2,3,4]))
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+        
+        return result
+
+
+print(Solution.productExceptSelfThree(Solution, [-1,2,3,4]))
