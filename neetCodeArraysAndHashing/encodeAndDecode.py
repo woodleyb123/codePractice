1e4e61708075324c6c8c2935eac8e5f3b39981fa
woodleyb123 <woodley.burrow1@gmail.com>
2025-01-28 21:10:27 -0500

    folder reorg

---
+++ neetCodeArraysAndHashing/encodeAndDecode.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return 'hi'
+    
+    def decode(self, s: str) -> List[str]:
+        return 'o'
+    
